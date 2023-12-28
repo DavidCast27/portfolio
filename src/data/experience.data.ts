a210c8d0bc58921543e0927b645cf2d0fa7a0a17
David Castrillon <david.cast27@gmail.com>
2023-12-28 14:25:49 -0500

    add experiencie and about me mockup

---
+++ src/data/experience.data.ts
@@ -0,0 +1,26 @@
+export const EXPERIENCES = [
+  {
+    date: "marzo 2023",
+    title: "Contekta",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    actual: true,
+  },
+  {
+    date: "marzo 2023",
+    title: "Mercado libre",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    actual: false,
+  },
+  {
+    date: "marzo 2023",
+    title: "bancolombia",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    actual: false,
+  },
+  {
+    date: "marzo 2020",
+    title: "Accenture",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    actual: false,
+  },
+];
---
+++ src/data/projects.data.ts
@@ -0,0 +1,10 @@
+export const PROJECTS = [{
+  title: 'mi primer proyecto',
+  description: 'Reprehenderit sit aliqua tempor culpa sint officia ad. Culpa cupidatat dolore amet ea. Ut proident mollit dolore do in nulla anim Lorem.',
+  repo: "",
+  link: "",
+  image: "https://via.placeholder.com/300x200",
+  tags: ['HTML', 'CSS', 'js'],
+}
+
+]
