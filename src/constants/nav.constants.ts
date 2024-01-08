2b46ce9f2356a846cfefc5445b44ed089bdb2984
David Castrillon <david.cast27@gmail.com>
2024-01-08 00:31:41 -0500

    fix: light dark mode

---
+++ src/constants/nav.constants.ts
@@ -0,0 +1,6 @@
+export const HEADER_ITEMS = [
+  { label: "Inicio", href: "#home" },
+  { label: "Experiecia", href: "#experience" },
+  { label: "Proyectos", href: "#projects" },
+  { label: "Sobre mi", href: "#about-me" },
+];
