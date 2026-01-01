f126395eddd062951311332b5e854a3b28a9ce03
David Castrillon <david.cast27@gmail.com>
2026-01-01 16:30:32 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  site: 'https://castri.dev',
+  integrations: [preact(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
